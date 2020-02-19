80b28269eea066b080e6c9dc760ea75fb0e3fa0b
Shiva R. Iyer <shiva.iyer@cs.nyu.edu>
2020-02-18 19:13:59 -0500

    Remove shell=True option in Popen

--- runmm.py
+++ runmm.py
@@ -1,4 +1,5 @@
 import os, sys
+import signal
 import argparse
 import threading
 import subprocess
@@ -6,11 +7,11 @@ import subprocess
 from time import sleep
 
 def RunSim():
-    server_ip = "100.64.0.1" # MAHIMAHI_BASE
+    server_ip = "100.64.0.1" # MAHIMAHI_BASE: address exposed by native machine to process inside mahimahii 
     #server_ip = args.servAddr
     #port = '5999'
-    mtu_bytes = '1500'
-    bw_mbps = args.bandwidth
+    #mtu_bytes = '1500'
+    #bw_mbps = args.bandwidth
     ttr = str(args.time)
     probe = 1
     
@@ -22,23 +23,26 @@ def RunSim():
     
     if args.queue is not None:
         saveprefix += '_q{}'.format(args.queue)
-                        
+    
     # starting the server receiver in a separate process
-    recvlogfpath = os.path.join(args.dir, saveprefix + '_receiver.log')
-    receiver_cmd = 'sender_receiver/receiver {} {} 1 &'.format(args.port, recvlogfpath)
+    recvlogfpathprefix = os.path.join(args.dir, saveprefix + '_receiver')
+    receiver_cmd = 'sender_receiver/receiver {} {} 1'.format(args.port, recvlogfpathprefix) # removed "&"
     print('Receiver run using command:', receiver_cmd)
-    receiver_process = subprocess.Popen(receiver_cmd, shell=True)
+    receiver_process = subprocess.Popen(receiver_cmd.split()) # shell=True
     print('Receiver started, pid', receiver_process.pid)
     
     # starting the mahimahi router in a separate process
     mm_cmd = "mm-link traces/channels/{0} traces/channels/{0} --uplink-log {1}/{2}_uplink.csv --downlink-log {1}/{2}_downlink.csv".format(args.trace, args.dir, saveprefix)
     if args.queue is not None:
         mm_cmd += " --uplink-queue=droptail --uplink-queue-args=bytes={}".format(args.queue)
+    
     print('Mahimahi run using command:', mm_cmd)
-    mm_process = subprocess.Popen(mm_cmd, stdin=subprocess.PIPE, universal_newlines=True, shell=True) # add text=True if required (may be required in more recent python3 versions such as python 3.7.x
+    mm_process = subprocess.Popen(mm_cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True) # shell=True # add text=True if required (may be required in more recent python3 versions such as python 3.7.x
     
     # to ensure the mm interface gets up
-    sleep(.5)
+    sleep(5)
+    
+    print('Mahimahi process started, pid', mm_process.pid)
     
     # starting the user sender
     senderlogfpath = os.path.join(args.dir, saveprefix + '_sender.log')
@@ -49,23 +53,21 @@ def RunSim():
         sender_cmd += 'file ' + args.filepath
     
     print(sender_cmd)
-    stdout, stderr = mm_process.communicate(sender_cmd)
-
-    sleep(args.time)
-    
+    stdout, _ = mm_process.communicate(sender_cmd)
+    print('mm_process.communicate() has returned.')
     print('stdout:', stdout)
-    print('stderr:', stderr)
+    
+    # sleep(args.time)
     
     # waiting for processes to close
-    print('Waiting for server process to close.')
-    # receiver_process.terminate()
-    # print(receiver_process.wait())
-    # subprocess.call("kill {}".format(receiver_process.pid), shell=True)
-    subprocess.call("pkill receiver", shell=True)
-    print('Done!')
     
     print('Waiting for mahimahi to close.')
-    mm_process.wait()
+    print('Mahimahi process returned', mm_process.wait())
+    print('Done!')
+    
+    print('Waiting for server process to close.')
+    receiver_process.send_signal(signal.SIGINT)
+    print('Server process returned', receiver_process.wait())
     print('Done!')
     
     # subprocess.call("sudo pkill sender_sender", shell=True)
