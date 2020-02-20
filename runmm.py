ca9b410b5d95bc17b3904a9928cc3f865310e56f
Shiva R. Iyer <shiva.iyer@cs.nyu.edu>
2020-02-20 16:45:36 -0500

    Make receiver run inside mahimahi rather than sender
    There seems to be a bug with mahimahi where tcp packets from sender
    to receiver when sender is inside mahimahi do not get sent, when
    the receiver is started alongside mm in the same command. This was
    fixed by making receiver run inside mm instead of sender.

--- runmm.py
+++ runmm.py
@@ -7,7 +7,8 @@ import subprocess
 from time import sleep
 
 def RunSim():
-    server_ip = "100.64.0.1" # MAHIMAHI_BASE: address exposed by native machine to process inside mahimahii 
+    server_ip = "100.64.0.1" # MAHIMAHI_BASE: address exposed by native machine to process inside mahimahii
+    server_reverse_ip = "100.64.0.2" # address exposed by mahimahi to process outsidexs
     #server_ip = args.servAddr
     #port = '5999'
     #mtu_bytes = '1500'
@@ -24,54 +25,68 @@ def RunSim():
     if args.queue is not None:
         saveprefix += '_q{}'.format(args.queue)
     
-    # starting the server receiver in a separate process
+    # (1) starting the server receiver in a separate process inside a mahimahi shell
     recvlogfpathprefix = os.path.join(args.dir, saveprefix + '_receiver')
-    receiver_cmd = 'sender_receiver/receiver {} {} 1'.format(args.port, recvlogfpathprefix) # removed "&"
-    print('Receiver run using command:', receiver_cmd)
-    receiver_process = subprocess.Popen(receiver_cmd.split()) # shell=True
-    print('Receiver started, pid', receiver_process.pid)
+    receiver_cmd = ' sender_receiver/receiver {} 1 {} 1'.format(args.port, recvlogfpathprefix) # removed "&"
+    #print('Receiver run using command:', receiver_cmd)
     
-    # starting the mahimahi router in a separate process
     mm_cmd = "mm-link traces/channels/{0} traces/channels/{0} --uplink-log {1}/{2}_uplink.csv --downlink-log {1}/{2}_downlink.csv".format(args.trace, args.dir, saveprefix)
     if args.queue is not None:
         mm_cmd += " --uplink-queue=droptail --uplink-queue-args=bytes={}".format(args.queue)
     
-    print('Mahimahi run using command:', mm_cmd)
-    mm_process = subprocess.Popen(mm_cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True) # shell=True # add text=True if required (may be required in more recent python3 versions such as python 3.7.x
+    mm_cmd += receiver_cmd
+    print('Starting receiver inside Mahimahi using command:', mm_cmd)
+    mm_process = subprocess.Popen(mm_cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+    #mm_process = subprocess.Popen(mm_cmd.split(), universal_newlines=True)
     
     # to ensure the mm interface gets up
-    sleep(5)
+    sleep(1)
     
     print('Mahimahi process started, pid', mm_process.pid)
     
+    #receiver_process = subprocess.Popen(receiver_cmd.split()) # shell=True
+    #print('Receiver started, pid', receiver_process.pid)
+    
+    # starting the mahimahi router in a separate process
+    
     # starting the user sender
     senderlogfpath = os.path.join(args.dir, saveprefix + '_sender.log')
-    sender_cmd = 'sender_receiver/sender {} {} {} {} --type '.format(server_ip, args.port, ttr, senderlogfpath)
+    sender_cmd = 'sender_receiver/sender {} {} {} {} --type '.format(server_reverse_ip, args.port, ttr, senderlogfpath)
     if args.const is not None:
         sender_cmd += 'const {} {}'.format(args.const[0], args.const[1])
     elif args.filepath is not None:
         sender_cmd += 'file ' + args.filepath
     
-    print(sender_cmd)
-    stdout, _ = mm_process.communicate(sender_cmd)
-    print('mm_process.communicate() has returned.')
-    print('stdout:', stdout)
+    #print(sender_cmd)
+    #stdout, _ = mm_process.communicate(sender_cmd)
+    #print('mm_process.communicate() has returned.')
+    #print('stdout:', stdout)
+    
+    print('Starting sender using command:', sender_cmd)
+    sender_process = subprocess.Popen(sender_cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    #sender_process = subprocess.Popen(sender_cmd.split())
+    print('Sender started, pid', sender_process.pid)
     
     # sleep(args.time)
+    #sleep(5)
     
     # waiting for processes to close
+
+    print('Waiting for sender process ...')
+    print('Sender process returned', sender_process.wait())
     
-    print('Waiting for mahimahi to close.')
+    print('Waiting receiver process ...')
+    #mm_process.send_signal(signal.SIGINT)
     print('Mahimahi process returned', mm_process.wait())
-    print('Done!')
     
-    print('Waiting for server process to close.')
-    receiver_process.send_signal(signal.SIGINT)
-    print('Server process returned', receiver_process.wait())
-    print('Done!')
+    # print('Waiting for server process to close.')
+    # receiver_process.send_signal(signal.SIGINT)
+    # print('Server process returned', receiver_process.wait())
+    # print('Done!')
     
     # subprocess.call("sudo pkill sender_sender", shell=True)
     # subprocess.call("sudo pkill -INT server_receiver", shell=True)
+    print('Done!')
     
     return
 
