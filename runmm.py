5a82e6aef26f178b172c67545efc820789765330
Shiva R. Iyer <shiva.iyer@cs.nyu.edu>
2020-03-23 18:20:23 -0400

    runmm.py: Clean up commandline args and general code
    - time_rate arg added but not implemented
    
    - incorporated changes to sender/receiver (args)
    
    - fixed minor formatting and typos

--- runmm.py
+++ runmm.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+#from __future__ import print_function
 
 import os, sys
 import signal
@@ -9,14 +9,9 @@ import subprocess
 from time import sleep
 
 def RunSim():
-    server_ip = "100.64.0.1" # MAHIMAHI_BASE: address exposed by native machine to process inside mahimahii
-    server_reverse_ip = "100.64.0.2" # address exposed by mahimahi to process outsidexs
+    server_ip = "100.64.0.1" # MAHIMAHI_BASE: address exposed by native machine to processes inside mahimahi
+    server_reverse_ip = "100.64.0.2" # address exposed by mahimahi to processes outside
     #server_ip = args.servAddr
-    #port = '5999'
-    #mtu_bytes = '1500'
-    #bw_mbps = args.bandwidth
-    ttr = str(args.time)
-    probe = 1
     
     if args.const is not None:
         saveprefix = '{}_const_{}_{}'.format(args.trace, args.const[0], args.const[1])
@@ -28,7 +23,7 @@ def RunSim():
         saveprefix += '_q{}'.format(args.queue)
     
     # (1) starting the server receiver in a separate process inside a mahimahi shell
-    receiver_cmd = ' -- sender_receiver/receiver {} {} {}'.format(args.port, args.maxflows, args.verbose) # removed "&"
+    receiver_cmd = ' -- sender_receiver/receiver {} {} {:d}'.format(args.port, args.maxflows, args.verbose) # removed "&"
     if args.recvlog:
         recvlogdir = os.path.join(args.dir, saveprefix + '_receiver')
         if not os.path.exists(recvlogdir):
@@ -37,14 +32,14 @@ def RunSim():
         receiver_cmd += ' --log {}'.format(recvlogfpathprefix)
     #print('Receiver run using command:', receiver_cmd)
     
-    mm_cmd = "mm-link traces/channels/{0} traces/channels/{0} --uplink-log {1}/{2}_uplink.csv --downlink-log {1}/{2}_downlink.csv".format(args.trace, args.dir, saveprefix)
+    mm_cmd = "mm-link {0} {0} --uplink-log {1}_uplink.csv --downlink-log {1}_downlink.csv".format(os.path.join('traces', 'channels', args.trace), os.path.join(args.dir, saveprefix))
     if args.queue is not None:
         mm_cmd += " --uplink-queue=droptail --uplink-queue-args=bytes={}".format(args.queue)
     
     mm_cmd += receiver_cmd
     print('Starting receiver inside Mahimahi using command:', mm_cmd)
-    mm_process = subprocess.Popen(mm_cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
-    #mm_process = subprocess.Popen(mm_cmd.split(), universal_newlines=True)
+    #mm_process = subprocess.Popen(mm_cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+    mm_process = subprocess.Popen(mm_cmd.split(), universal_newlines=True)
     
     # to ensure the mm interface gets up
     print('Waiting for mm-interface to get up ...')
@@ -59,7 +54,7 @@ def RunSim():
     
     # starting the user sender
     senderlogfpath = os.path.join(args.dir, saveprefix + '_sender.log')
-    sender_cmd = 'sender_receiver/sender {} {} {} {} --type '.format(server_reverse_ip, args.port, ttr, senderlogfpath)
+    sender_cmd = 'sender_receiver/sender {} {} {} --type '.format(server_reverse_ip, args.port, senderlogfpath)
     if args.const is not None:
         sender_cmd += 'const {} {}'.format(args.const[0], args.const[1])
     elif args.filepath is not None:
@@ -106,6 +101,7 @@ def unsigned_int(arg):
         raise argparse.ArgumentError('Argument must be a nonnegative integer')
     return arg
 
+
 def positive_int(arg):
     arg = int(arg)
     if arg <= 0:
@@ -121,38 +117,31 @@ if __name__ == '__main__':
     #     		help="Server address, make it match with a local interface different from localhost",
     #     		required=True)
 
-    parser.add_argument('--port',
+    parser.add_argument('trace', metavar='CHANNEL_TRACE', 
+                        help='Cellsim traces to be used (channel simulation; choose one from traces/channels/)')
+    
+    parser.add_argument('--port', '-p', type=int, 
                         help='Port number to use',
-                        required=True)
+                        default=9999)
     
-    parser.add_argument('--trace', '-tr',
-			help='Cellsim traces to be used (channel simulation; choose one from traces/channels/)',
-			required=True)
+    parser.add_argument('--dir', '-d', 
+                        help='Directory to store outputs',
+                        default='output')
     
-    parser.add_argument('--dir', '-d',
-			help="Directory to store outputs",
-			default='output')
+    parser.add_argument('--queue', type=positive_int,
+                        help='Queue size in mahimahi (bytes)')
     
-    parser.add_argument('--time', '-t',
-			help="Duration (sec) to run the experiment",
-			type=int,
-			default=10)
-    
-    parser.add_argument('--bandwidth', '-bw',
-			help='Bandwidth in MiBps',
-			default='13')
-    
-    parser.add_argument('--queue', type=positive_int, help='Queue size in mahimahi (bytes)')
-
     megroup = parser.add_mutually_exclusive_group()
-
-    megroup.add_argument('--const', nargs=2,
-                         metavar=('NUM_PACKETS', 'PROBE_MODE'))
-        
-    megroup.add_argument('--file', '-f',
-                         dest='filepath',
-                         help="File from which to generate user sender trace")
-
+    
+    megroup.add_argument('--const', nargs=2, metavar=('NUM_PACKETS', 'PROBE_MODE'),
+                         help='A single flow with specified number of packets. Probe mode is 0 (test cc) or 1 (default cc).')
+    
+    megroup.add_argument('--time-rate', '-t', nargs=2, metavar=('DURATION_SEC', 'BW_MiBps'), 
+                        help='Duration (sec) to run the experiment with sending rate (# bytes per second)')
+    
+    megroup.add_argument('--file', '-f', dest='filepath',
+                         help='File from which to generate user sender trace')
+    
     parser.add_argument('--maxflows', type=unsigned_int,
                         help='Limit on number of flows in the simulation (0 for unlimited)',
                         default=1)
@@ -160,17 +149,20 @@ if __name__ == '__main__':
     parser.add_argument('--verbose', '-v', action='store_true',
                         help='Log progress on console',
                         default=False)
-
+    
     parser.add_argument('--recvlog', action='store_true',
                         help='Log packets at the receiver',
                         default=False)
     
     args = parser.parse_args()
     
+    if args.time_rate is not None:
+        raise argparse.ArgumentError('\"--time-rate\" is not implemented for now.')
+    
     # make output directories
     if not os.path.exists(args.dir):
         os.makedirs(args.dir)
-        
+    
     RunSim()
     
     print("Finished")
