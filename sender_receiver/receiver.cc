49f15dbd14aa0e1b14fe6f344b8e4bb3de3a0a13
Shiva R. Iyer <shiva.iyer@cs.nyu.edu>
2020-04-06 17:16:39 -0400

    receiver.cc/fix: Correct timeval_subtract to not modify inputs
    timeval_subtract was modifying the input arguments, because of which
    we kept seeing -ve microseconds in the final output for sent_time
    field.

--- sender_receiver/receiver.cc
+++ sender_receiver/receiver.cc
@@ -40,25 +40,26 @@ handle_sigchld(int signum) {
 int
 timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
 {
-  /* Perform the carry for the later subtraction by updating y. */
-  if (x->tv_usec < y->tv_usec) {
-    int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-    y->tv_usec -= 1000000 * nsec;
-    y->tv_sec += nsec;
+  // Perform the carry for the later subtraction
+  long y_sec = y->tv_sec, y_usec = y->tv_usec;
+  
+  if (x->tv_usec < y_usec) {
+    int nsec = (y_usec - x->tv_usec) / 1000000 + 1;
+    y_usec -= 1000000 * nsec;
+    y_sec += nsec;
   }
-  if (x->tv_usec - y->tv_usec > 1000000) {
-    int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-    y->tv_usec += 1000000 * nsec;
-    y->tv_sec -= nsec;
+  if (x->tv_usec - y_usec > 1000000) {
+    int nsec = (x->tv_usec - y_usec) / 1000000;
+    y_usec += 1000000 * nsec;
+    y_sec -= nsec;
   }
   
-  /* Compute the time remaining to wait.
-     tv_usec is certainly positive. */
-  result->tv_sec = x->tv_sec - y->tv_sec;
-  result->tv_usec = x->tv_usec - y->tv_usec;
+  // now subtract
+  result->tv_sec = x->tv_sec - y_sec;
+  result->tv_usec = x->tv_usec - y_usec;
   
-  /* Return 1 if result is negative. */
-  return x->tv_sec < y->tv_sec;
+  // Return 1 if result is negative. 
+  return x->tv_sec < y_sec;
 }
 
 
